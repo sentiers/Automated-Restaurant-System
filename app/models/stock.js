42372e81e9ee5e49ffce6fd7913678dbfe31ee8a
Hyunji Cho <sentiersg@gmail.com>
2021-11-04 20:17:24 +0900

    Update order.js, stock.js

--- app/models/stock.js
+++ app/models/stock.js
@@ -3,7 +3,15 @@ var Schema = mongoose.Schema;
 
 // 재고 스키마
 var StockSchema = new Schema({
-    
+    // stock_id
+    stock_name: String, // 재고 이름
+    stock_quantity: Number, // 현재 수량
+    stock_order_info:{ // 발주 정보
+        order_company: String, // 납품업체명
+        order_unit: String, // 단위
+        order_price: Number, // 가격
+        order_phone: String // 전화번호
+    }
 });
 
 var Stock = mongoose.model('Stock', StockSchema);
