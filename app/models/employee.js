f47d1c5cb93cd5cf3bd5068fcea8aa445140cda3
Moon <moonsc014@gmail.com>
2021-11-04 23:50:15 +0900

    Update employee.js, menu.js

--- app/models/employee.js
+++ app/models/employee.js
@@ -3,7 +3,18 @@ var Schema = mongoose.Schema;
 
 // 직원 스키마
 var EmployeeSchema = new Schema({
-
+  // employee_id
+  employee_name: String, // 이름
+  employee_contact: String, // 전화번호
+  employee_field: String, // 직무
+  employee_status: Number, // 현재 상태 출근 = 1, 퇴근 = 0
+  employee_record: [
+    {
+      recordDate: Date, // 기록 날짜
+      attend: String, // 출근 시간
+      off: String, //퇴근 시간
+    },
+  ],
 });
 
 var Employee = mongoose.model('Employee', EmployeeSchema);
--- app/models/menu.js
+++ app/models/menu.js
@@ -4,7 +4,18 @@ var ObjectId = require('mongodb').ObjectID;
 
 // 메뉴 스키마
 var MenuSchema = new Schema({
-
+  // menu_id
+  menu_name: String,
+  menu_price: Number,
+  menu_calorie: Number,
+  menu_img: String,
+  menu_info: String,
+  menu_ingr: [
+    {
+      ingr_name: String, // 재료 이름
+      ingr_num: Number, // 재료 수량
+    },
+  ],
 });
 
 var Menu = mongoose.model('Menu', MenuSchema);
