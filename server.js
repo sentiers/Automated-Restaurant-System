461edef36510b2a82caf62d58f869ab4bd8dc612
Hyunji Cho <sentiersg@gmail.com>
2021-11-08 16:27:16 +0900

    Create employeepage.js

--- server.js
+++ server.js
@@ -2,15 +2,18 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 //==== 설정 =================
 app.use(express.static(path.join(__dirname, 'app')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //==== 몽고디비 연결 ==================================
 var url =
   'mongodb+srv://dbUser:5star@restaurantmanagement.uq227.mongodb.net/5starDatabase?retryWrites=true&w=majority';
-  
+
 mongoose
   .connect(url, {
     useNewUrlParser: true,
@@ -21,9 +24,11 @@ mongoose
 
 //==== ROUTES ================================================
 var root = require(__dirname + '/app/routes/root');
+var employeepage = require(__dirname + '/app/routes/employeepage');
 
 //==== ROUTING ===============================================
 app.use('/', root);
+app.use('/employeepage', employeepage);
 
 //==== LISTEN TO THE SERVER =================================
 app.listen(process.env.PORT || 8080, () =>
