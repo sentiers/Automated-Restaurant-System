0f1079a9a980f4e86e6485cd14b4cdd12b786c9b
Hyunji Cho <sentiersg@gmail.com>
2021-11-17 22:04:53 +0900

    Merge salespage routes

--- app/routes/salespage.js
+++ app/routes/salespage.js
@@ -1,11 +1,50 @@
 // --------------------------------------------------------
 var router = require('express').Router();
 var path = require('path');
+var express = require('express');
+var Order = require('../models/order');
+router.use(express.static('./app'));
 // --------------------------------------------------------
 
-//====  =============================
+
+//==== 모든 주문 가져오기 =========================
+function getAllOrder() {
+    return new Promise(function (resolve, reject) {
+        Order.find()
+            .then(data => {
+                resolve(data);
+            }).catch((err) => {
+                reject(500);
+            });
+    });
+};
+
+
+// --------------------------------------------------------
+
+//====매출관리페이지 메인=============================
 router.get('/', function (req, res, next) {
-    res.sendFile(path.join(__dirname, "../views/sales.html"));
+    getAllOrder()
+        .then((data) => {
+            res.render('sales', { datas: data });
+        }).catch((errcode) => {
+            console.log("err");
+        });
+});
+
+//====월=============================
+router.get('/wol', function (req, res, next) {
+    res.render('sales_wol');
+});
+
+//==== 메뉴 =============================
+router.get('/menu', function (req, res, next) {
+    res.render('sales_menu');
+});
+
+//====주문 =============================
+router.get('/jumun', function (req, res, next) {
+    res.render('sales_jumun');
 });
 
 // --------------------------------------------------------
