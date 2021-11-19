9e5b61d1dbbc27c8eb8ada44e160a1b01a48bf02
kimmingyu0103 <90326161+kimmingyu0103@users.noreply.github.com>
2021-11-20 00:37:56 +0900

    Add files via upload

--- app/routes/salespage.js
+++ app/routes/salespage.js
@@ -3,6 +3,7 @@ var router = require('express').Router();
 var path = require('path');
 var express = require('express');
 var Order = require('../models/order');
+var Menu = require('../models/menu');
 router.use(express.static('./app'));
 // --------------------------------------------------------
 
@@ -15,6 +16,17 @@ function getAllOrder() {
                 resolve(data);
             }).catch((err) => {
                 reject(500);
+            });
+    });
+};
+
+function getAllMenuAndOrder() {
+    return new Promise(function (resolve, reject) {
+        Order.find()
+            .then(order => {
+                resolve(order);
+            }).catch((err) => {
+                reject(5001);
             });
     });
 };
@@ -34,17 +46,30 @@ router.get('/', function (req, res, next) {
 
 //====월=============================
 router.get('/wol', function (req, res, next) {
-    res.render('sales_wol');
-});
+    getAllOrder()
+        .then((data) => {
+            res.render('sales_wol', { datas: data });
+        }).catch((errcode) => {
+            console.log("err");
+        });});
 
 //==== 메뉴 =============================
 router.get('/menu', function (req, res, next) {
-    res.render('sales_menu');
+    Order.find({}, function (err, data) {
+        Menu.find({}, function (err, menu) {
+            res.render('sales_menu', {datas:data, menus: menu});
+        });
+    });
+    
 });
 
 //====주문 =============================
 router.get('/jumun', function (req, res, next) {
-    res.render('sales_jumun');
+    Order.find({}, function (err, data) {
+        Menu.find({}, function (err, menu) {
+            res.render('sales_jumun', {datas:data, menus: menu});
+        });
+    });
 });
 
 // --------------------------------------------------------
