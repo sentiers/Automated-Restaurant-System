d94a512119b9a7f66942be6d5c463c93df3612b3
Moon <moonsc014@gmail.com>
2021-11-24 12:24:31 +0900

    Create stock_order.ejs

--- app/routes/stockpage.js
+++ app/routes/stockpage.js
@@ -9,7 +9,7 @@ router.use(express.static('./app'));
 // --------------------------------------------------------
 
 //==== 모든 재고가져오기 =========================
-function getAllMenu() {
+function getAllStock() {
   return new Promise(function (resolve, reject) {
     Stock.find()
       .then((data) => {
@@ -22,7 +22,7 @@ function getAllMenu() {
 }
 
 //==== 재고 생성하는 함수 =========================
-function createMenu(data) {
+function createStock(data) {
   return new Promise(function (resolve, reject) {
     var newStock = new Stock(data);
     newStock.save((err) => {
@@ -37,7 +37,7 @@ function createMenu(data) {
 }
 
 //==== 재고 수정하는 함수 =========================
-function updateMenu(data, idData) {
+function updateStock(data, idData) {
   return new Promise(function (resolve, reject) {
     Stock.updateOne(
       { _id: idData },
@@ -62,7 +62,7 @@ function updateMenu(data, idData) {
 }
 
 //==== 재고 id 별로 조회 함수 =========================
-function getMenuById(idData) {
+function getStockById(idData) {
   return new Promise(function (resolve, reject) {
     Stock.findOne({
       _id: idData,
@@ -78,7 +78,7 @@ function getMenuById(idData) {
 }
 
 //==== 재고 삭제 함수 =========================
-function removeMenu(idData) {
+function removeStock(idData) {
   return new Promise(function (resolve, reject) {
     Stock.deleteOne({
       _id: idData,
@@ -93,11 +93,26 @@ function removeMenu(idData) {
   });
 }
 
+//==== 재고 발주 함수 =========================
+function orderStock(idData) {
+  return new Promise(function (resolve, reject) {
+    Stock.findOne({
+      _id: idData,
+    })
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((err) => {
+        // 재고를 찾을수없을때
+        reject(404);
+      });
+  });
+}
 // ----------------------------------------------------------------
 
 //==== 전체 재고 조회 =============================
 router.get('/', function (req, res, next) {
-  getAllMenu()
+  getAllStock()
     .then((data) => {
       res.render('stock', { datas: data });
     })
@@ -112,7 +127,7 @@ router.get('/create', function (req, res, next) {
 });
 
 router.post('/create', function (req, res, next) {
-  createMenu(req.body)
+  createStock(req.body)
     .then(() => {
       res.redirect('/stockpage');
     })
@@ -123,7 +138,7 @@ router.post('/create', function (req, res, next) {
 
 //==== id와 일치하는 재고 조회 =============================
 router.get('/:id', function (req, res, next) {
-  getMenuById(req.params.id)
+  getStockById(req.params.id)
     .then((data) => {
       res.render('stock_view', data);
     })
@@ -134,7 +149,7 @@ router.get('/:id', function (req, res, next) {
 
 //==== 재고수정 =============================
 router.post('/update/:id', function (req, res, next) {
-  updateMenu(req.body, req.params.id)
+  updateStock(req.body, req.params.id)
     .then(() => {
       res.redirect('/stockpage');
     })
@@ -145,9 +160,21 @@ router.post('/update/:id', function (req, res, next) {
 
 //==== 재고 삭제 =============================
 router.get('/remove/:id', function (req, res, next) {
-  removeMenu(req.params.id)
+  removeStock(req.params.id)
     .then(() => {
       res.redirect('/stockpage');
+    })
+    .catch((errcode) => {
+      console.log(errcode);
+    });
+});
+
+//==== 재고 발주 =============================
+router.get('/order/:id', function (req, res, next) {
+  getStockById(req.params.id)
+    .then((data) => {
+      res.render('stock_order', data);
+      //res.redirect('/stockpage');
     })
     .catch((errcode) => {
       console.log(errcode);
