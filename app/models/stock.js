bec128864abf2c2a32ba307e94134fd0b91257b9
Moon <moonsc014@gmail.com>
2021-11-25 00:20:46 +0900

    Update Stock.js

--- app/models/stock.js
+++ app/models/stock.js
@@ -3,15 +3,25 @@ var Schema = mongoose.Schema;
 
 // 재고 스키마
 var StockSchema = new Schema({
-    // stock_id
-    stock_name: String, // 재고 이름
-    stock_quantity: Number, // 현재 수량
-    stock_order:{ // 발주 정보
-        order_company: String, // 납품업체명
-        order_unit: String, // 단위
-        order_price: Number, // 가격
-        order_phone: String // 전화번호
-    }
+  // stock_id
+  stock_name: String, // 재고 이름
+  stock_quantity: Number, // 현재 수량
+  stock_type: String,
+  stock_order: {
+    // 발주 정보
+    order_company: String, // 납품업체명
+    order_unit: Number, // 단위
+    order_price: Number, // 가격
+    order_phone: String, // 전화번호
+    order_status: {
+      type: Number,
+      default: 0,
+    }, // 주문 상태 0-접수없음 1-배송중 2-배송완료
+    order_quantity: {
+      type: Number,
+      default: 0,
+    }, // 주문 수량
+  },
 });
 
 var Stock = mongoose.model('Stock', StockSchema);
