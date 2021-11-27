401fddb55948af7d2b6bbe4a9083d849ea633e4d
Hyunji Cho <sentiersg@gmail.com>
2021-11-27 23:19:55 +0900

    Update root.js for deducting stocks

--- app/routes/root.js
+++ app/routes/root.js
@@ -3,19 +3,21 @@ var router = require('express').Router();
 var express = require('express');
 var Menu = require('../models/menu');
 var Order = require('../models/order');
+var Stock = require('../models/stock');
+var delay = require('delay');
 router.use(express.static('./app'));
 // --------------------------------------------------------
 //==== 모든 메뉴 가져오기 =========================
 function getAllMenu() {
-    return new Promise(function (resolve, reject) {
-      Menu.find()
-        .then((data) => {
-          resolve(data);
-        })
-        .catch((err) => {
-          reject(500);
-        });
-    });
+  return new Promise(function (resolve, reject) {
+    Menu.find()
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((err) => {
+        reject(500);
+      });
+  });
 };
 //==== 모든 주문 가져오기 =========================
 function getAllOrder() {
@@ -33,69 +35,120 @@ function getAllOrder() {
 function createOrder(data) {
   data.order_list = JSON.parse(data.order_list);
   return new Promise(function (resolve, reject) {
-      var newOrder = new Order(data);
-      newOrder.save((err) => { // 주문 저장
-          if (err) {
-              reject(500);
-          } else {
-              resolve(201);
-          }
-      });
+    var newOrder = new Order(data);
+    newOrder.save((err) => { // 주문 저장
+      if (err) {
+        reject(500);
+      } else {
+        resolve(201);
+      }
+    });
   });
 };
-//==== 주문 수정하는 함수 =========================
+//==== 주문 완료하는 함수 =========================
 function updateOrder(data, idData) {
   return new Promise(function (resolve, reject) {
-      Order.updateOne(
-          { _id: idData },
-          {
-              $set: {
-                  order_state: data.order_state
-              }
-          }
-      ).then(() => {
-          resolve(200);
-      }).catch((err) => { // 직원을 찾을수없을때
-          reject(404);
-      });
+    Order.updateOne(
+      { _id: idData },
+      {
+        $set: {
+          order_state: data.order_state
+        }
+      }
+    ).then(() => {
+      resolve(200);
+    }).catch((err) => { // 직원을 찾을수없을때
+      reject(404);
+    });
   });
 };
+
+//==== 재고 차감하는 함수 =========================
+function updateStock(idData) {
+  return new Promise(function (resolve, reject) {
+    Order.findOne({ _id: idData }
+    ).then((order) => {
+
+      var findMenu = async (orderlists) => {
+        for (const orderlist of orderlists) { // 메뉴당 루프
+          await delay().then(() => { // 루프를 위한 딜레이
+            Menu.findOne(
+              { _id: orderlist.menu }
+            ).then((menu) => {
+              findStock(menu.menu_ingr, orderlist.quantity)
+            })
+          })
+        }
+      }
+
+      var findStock = async (ingrlists, quantity) => {
+        for (const ingr of ingrlists) { // 재고당 루프
+          await delay().then(() => { // 루프를 위한 딜레이
+            Stock.updateOne(
+              { stock_name: ingr.ingr_name },
+              {
+                $inc: {
+                  stock_quantity: - (ingr.ingr_quantity * quantity)
+                }
+              }
+            ).exec();
+          })
+        }
+      }
+
+      findMenu(order.order_list).then(() => { // 실행하는 부분
+        resolve(200);
+      });
+
+    }).catch((err) => { // 직원을 찾을수없을때
+      console.log(err);
+      reject(404);
+    });
+  });
+};
+
+// --------------------------------------------------------
 
 //==== 메인 =============================
 router.get('/', function (req, res, next) {
-    res.redirect("/main");
+  res.redirect("/main");
 });
 
 router.get('/main', function (req, res, next) {
-    getAllMenu()
-        .then((Menudata) => {
-          getAllOrder()
-          .then((Orderdata) => {
-            res.render('main', { Menudatas: Menudata, Orderdatas: Orderdata });
-          }).catch((errcode) => {
-            console.log("err");
-          });
+  getAllMenu()
+    .then((Menudata) => {
+      getAllOrder()
+        .then((Orderdata) => {
+          res.render('main', { Menudatas: Menudata, Orderdatas: Orderdata });
         }).catch((errcode) => {
-            console.log("err");
+          console.log("err");
         });
+    }).catch((errcode) => {
+      console.log("err");
+    });
 });
 //==== 주문 생성 =============================
 router.post('/main/createOrder', function (req, res, next) {
   createOrder(req.body)
-      .then(() => {
-          res.redirect('/main');
-      }).catch((errcode) => {
-          console.log(errcode);
-      });
+    .then(() => {
+      res.redirect('/main');
+    }).catch((errcode) => {
+      console.log(errcode);
+    });
 });
-//==== 주문 수정 =============================
+//==== 주문 완료 =============================
 router.post('/main/update/:id', function (req, res, next) {
   updateOrder(req.body, req.params.id)
-      .then(() => {
+    .then(() => {
+      updateStock(req.params.id)
+        .then(() => {
           res.redirect('/main');
-      }).catch((errcode) => {
+        }).catch((errcode) => {
           console.log(errcode);
-      });
+        });
+    }).catch((errcode) => {
+      console.log(errcode);
+    });
 });
 // --------------------------------------------------------
 
